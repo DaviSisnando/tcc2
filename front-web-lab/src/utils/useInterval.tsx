ef8080cf6ce7eb22d70f6c48acb99b9d3c65b71f
Davi Sisnando <davi_sisnando@hotmail.com>
2023-06-19 09:01:34 -0300

    fix: added optional chain on tick function

--- front-web-lab/src/utils/useInterval.tsx
+++ front-web-lab/src/utils/useInterval.tsx
@@ -9,7 +9,7 @@ export function useInterval(callback: () => void, delay: number | null) {
 
   useEffect(() => {
     function tick() {
-      savedCallback.current && savedCallback.current();
+      savedCallback?.current && savedCallback?.current();
     }
     if (delay !== null) {
       const id = setInterval(tick, delay);
